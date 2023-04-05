6a0febfd4fb3d107de7179cbab2370ff4321411d
daddyloyal <lifeofemmany6@gmail.com>
2023-04-05 11:02:30 +0100

    CRUD and template filter

--- events/models.py
+++ events/models.py
@@ -9,3 +9,6 @@ class Venue(models.Model):
 
     def __str__(self):
         return self.name
+
+    class Meta:
+        verbose_name_plural = ('venue')
--- events/views.py
+++ events/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Venue
 
 
 def home(request):
-    return render(request, 'events/home.html')
+    venues = Venue.objects.all()
+    return render(request, 'events/home.html', {'venues': venues})
 
 
 def about(request):
