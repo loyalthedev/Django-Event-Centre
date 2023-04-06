2591b9ee4f24b5f0492df50d6a2bd4ba99ded575
daddyloyal <lifeofemmany6@gmail.com>
2023-04-06 13:17:08 +0100

    Added read more routing

--- events/urls.py
+++ events/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('contact/', views.contact, name='contact'),
+    path('<int:venue_id>/', views.read_view, name='read'),
 ]
--- events/views.py
+++ events/views.py
@@ -4,8 +4,13 @@ from .models import Venue
 
 
 def home(request):
-    venues = Venue.objects.all()
+    venues = Venue.objects.all().order_by('-id')
     return render(request, 'events/home.html', {'venues': venues})
+
+
+def read_view(request, venue_id):
+    venue = Venue.objects.get(id=venue_id)
+    return render(request, 'events/read_view.html', {'venue': venue})
 
 
 def about(request):
